349ce3706653d9528250ff7954404419cdc15422
Pierce <phicks21@gmail.com>
2018-10-19 07:15:49 -0400

    working app now

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import albumData from './../data/albums';
 
-const Library = () => (
-  <section className="library">
-    Library will go here
-  </section>
-);
+class Library extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {albums: albumData};
+  }
+
+  render() {
+    return (
+      <section className="Library">
+        {
+          this.state.albums.map((album, index) =>
+
+          <Link to={`/album/${album.slug}`} key={index}>
+           </Link>
+        )
+        }
+      </section>
+    );
+  }
+}
 
 
 export default Library;
