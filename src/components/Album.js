452c30f98937635b6bb2f3bca4ae378bd25d5395
Pierce <phicks21@gmail.com>
2018-12-10 22:30:00 -0500

    Update Album.js

--- src/components/Album.js
+++ src/components/Album.js
@@ -66,7 +66,6 @@ handleSongClick(song) {
             <tbody>
             {
               this.state.album.songs.map((song, index) =>
-
                 <tr className="song" key={index}
                    onClick={() => this.handleSongClick(song)}
                    onMouseEnter={() => this.setState({isHovered: index+1})}
