10209e5cbdc375aac45102a0cda46026bd4f9827
Pierce Hicks <phicks21@gmail.com>
2019-11-04 19:24:37 -0500

    vers correction

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { Icon, InlineIcon } from '@iconify/react';
+import iosSkipBackward from '@iconify/icons-ion/ios-skip-backward';
 
 class PlayerBar extends Component {
   render(){
@@ -8,6 +10,7 @@ class PlayerBar extends Component {
     <section id="buttons">
          <button id="previous" onClick={this.props.handlePrevClick}>
            <span className="ion-skip-backward"></span>
+           <Icon icon={iosSkipBackward} />
          </button>
          <button id="play-pause" onClick={this.props.handleSongClick}>
            <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
